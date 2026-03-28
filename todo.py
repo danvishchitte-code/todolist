15c240ae5641a64e7604ee89015aa812a53e7a60
danvishchitte-code <danvishchitte@gmail.com>
2026-03-28 17:59:13 +0530

    modified

--- todo.py
+++ todo.py
@@ -59,4 +59,13 @@ for i in range(1,n+1):
         print(j, end=" ")
     print()
 
-    
+a=65
+for i in range(1,n+1):
+    for j in range(1,i+1):
+        print(chr(a), end=" ")
+        a+=1
+    print()
+
+
+for letter in range(ord('A'), ord('Z')+1):
+    print(chr(letter), end=" ")
